2ff2ed25565b9a37d80b7680bf172f739f1524be
soren@thomson <scott.soren@gmail.com>
2020-03-02 20:41:29 +0100

    spectra.py and Zilien.py imported by __init__

--- __init__.py
+++ __init__.py
@@ -24,6 +24,8 @@ from EC_MS.Datapoints import *
 from EC_MS.Potentiostat import *
 from EC_MS.dataset import *
 from EC_MS.PVMassSpec import *
+from EC_MS.Zilien import *
 from EC_MS.patches import *
+from EC_MS.spectra import *
 import EC_MS.Chem as Chem
 
